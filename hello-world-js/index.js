24af376e876caf4389bd4dbbda3bb3ca64482923
Phat <nguyenphatit.nl@gmail.com>
2023-08-25 16:33:20 +0700

    first commit

---
+++ hello-world-js/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! (From Node.js v1)')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+/* To Handle Ctrl C*/
+process.on('SIGINT', function () {
+  process.exit();
+});
